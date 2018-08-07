7ed27d2b50f0ac0edd3b39fa711ad48b9e2d3d68
sNapz <noname@noname.com>
2018-08-07 08:47:18 +0200

    Revert "Revert "Debug 2 case where we have to off/on the bot for continue when it's stuck.""
    This reverts commit cc094a472247255db837cc5908425864956fea34.

--- config.js
+++ config.js
@@ -3,4 +3,19 @@ const mySignature = "Pwned by a french bot :>";
 const myBotName = "[BOT]sNapz_1";
 
 // Change the way to pick a victim, 0 = random || 1 = Focus the last || 2 = Focus the first
-const findVictimeType = 1;
+const findVictimeType = 0;
+
+/*
+BasicMiner = 0
+AdvancedMiner = 1
+MiningDrill = 2
+DataCenter = 3
+Botnet = 4
+QuantumServer = 5
+*/
+// Set the minimum type of miner we will buy
+// So if its 5 we will buy only QuantumServer
+// If its 4 we buy the max of QuantumServer then the max of Botnet
+// Etc... :D
+const minimumTypeOfMinerToBuy = 0;
+
